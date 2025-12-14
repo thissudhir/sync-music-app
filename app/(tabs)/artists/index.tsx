3db8c4615ce1565e3150b3428655b31f35a8006b
abhishek <abhishekworkhere@gmail.com>
2025-12-14 13:32:18 +0530

    UPDATE: git config for thissudhir

--- app/(tabs)/artists/index.tsx
+++ app/(tabs)/artists/index.tsx
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native'
 const ArtistsScreen = () => {
     return (
         <View style={defaultStyles.container}>
-            <Text style={defaultStyles.text}>ArtistsScreen</Text>
+            <Text style={defaultStyles.text}>Artists Screen </Text>
         </View>
     )
 }
