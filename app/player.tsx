3e82da4b104fbec62557e231565e9b2db6d423f5
Abhishek <abhishekworkhere@gmail.com>
2026-01-10 00:19:34 +0530

    ADD(hook): for background image of the song artwork

--- app/player.tsx
+++ app/player.tsx
@@ -1,16 +1,19 @@
 import { ActivityIndicator, Image, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { useActiveTrack } from 'react-native-track-player'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import { FontAwesome } from '@expo/vector-icons'
+import { LinearGradient } from 'expo-linear-gradient'
+
 import { defaultStyles, utilsStyles } from '@/styles'
 import { colors, fontSize, screenPadding } from '@/constants/tokens'
 import DismissPlayerSymbol from '@/components/DismissPlayerSymbol'
-import { useActiveTrack } from 'react-native-track-player'
-import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import MovingTitle from '@/components/MovingTitle'
-import { FontAwesome } from '@expo/vector-icons'
-import { PlayerControls, PlayPauseButton, SkipToNextButton, SkipToPreviousButton } from '@/components/PlayerControls'
+import { PlayerControls } from '@/components/PlayerControls'
 import PlayerProgressBar from '@/components/PlayerProgressBar'
 import PlayerVolumeBar from '@/components/PlayerVolumeBar'
 import PlayerToggleRepeat from '@/components/PlayerToggleRepeat'
+import { usePlayerBackground } from '@/hooks/usePlayerBackground'
 
 const PlayerScreen = () => {
     const { top, bottom } = useSafeAreaInsets()
@@ -20,6 +23,9 @@ const PlayerScreen = () => {
         console.log("Pressed the favroite button")
     }
     const activeTrack = useActiveTrack();
+
+    const { imageColors } = usePlayerBackground(activeTrack?.artwork ?? ' ')
+
     if (!activeTrack) {
         return (
             <View style={[defaultStyles.container, { justifyContent: "center" }]}>
@@ -27,72 +33,79 @@ const PlayerScreen = () => {
             </View>
         )
     }
+    console.log("image color", imageColors)
 
 
     return (
-        <View style={styles.overlayContainer}>
-            <DismissPlayerSymbol />
-            <View style={{ flex: 1, top: top + 70, marginBottom: bottom }}>
-                <View style={styles.artWorkImageContainer}>
+        <LinearGradient
+            style={{ flex: 1 }}
+            colors={imageColors ? [imageColors.background, imageColors.primary] : [colors.background, colors.background]}
+        >
+            <View style={styles.overlayContainer}>
+                <DismissPlayerSymbol />
+                <View style={{ flex: 1, top: top + 70, marginBottom: bottom }}>
+                    <View style={styles.artWorkImageContainer}>
 
-                    <Image
-                        style={styles.artWorkImage}
-                        source={{
-                            uri: activeTrack.artwork
-                        }}
-                    />
-                </View>
+                        <Image
+                            style={styles.artWorkImage}
+                            source={{
+                                uri: activeTrack.artwork
+                            }}
+                        />
+                    </View>
 
-                <View style={{ flex: 1 }}>
+                    <View style={{ flex: 1 }}>
 
-                    <View style={{ marginTop: 20 }}>
-                        <View style={{ height: 60, }}>
-                            <View
-                                style={{
-                                    flexDirection: 'row',
-                                    justifyContent: 'space-between',
-                                    alignItems: "center"
-                                }}>
-                                {/* TRACK TITLE  */}
-                                <View style={styles.trackTitleContainer}>
-                                    <MovingTitle
-                                        title={activeTrack.title ?? ""}
-                                        animatedThreshold={30}
-                                        style={styles.trackTitleText}
+                        <View style={{ marginTop: 20 }}>
+                            <View style={{ height: 60, }}>
+                                <View
+                                    style={{
+                                        flexDirection: 'row',
+                                        justifyContent: 'space-between',
+                                        alignItems: "center"
+                                    }}>
+                                    {/* TRACK TITLE  */}
+                                    <View style={styles.trackTitleContainer}>
+                                        <MovingTitle
+                                            title={activeTrack.title ?? ""}
+                                            animatedThreshold={30}
+                                            style={styles.trackTitleText}
+                                        />
+                                    </View>
+                                    {/* FAVROITE BUTTON  */}
+                                    <FontAwesome
+                                        name={isFavroite ? "heart" : 'heart-o'}
+                                        size={20}
+                                        color={isFavroite ? colors.primary : colors.icon}
+                                        style={{ marginHorizontal: 14 }}
+                                        onPress={toggleFavroite}
                                     />
                                 </View>
-                                {/* FAVROITE BUTTON  */}
-                                <FontAwesome
-                                    name={isFavroite ? "heart" : 'heart-o'}
-                                    size={20}
-                                    color={isFavroite ? colors.primary : colors.icon}
-                                    style={{ marginHorizontal: 14 }}
-                                    onPress={toggleFavroite}
-                                />
+                                {/* ARTIST NAME  */}
+                                {activeTrack.artist && (
+                                    <Text
+                                        numberOfLines={1}
+                                        style={[styles.trackArtistTrack, { marginTop: 1 }]}
+                                    >
+                                        {activeTrack.artist}
+                                    </Text>
+                                )}
+                                {/* CONTROLS  */}
+
                             </View>
-                            {/* ARTIST NAME  */}
-                            {activeTrack.artist && (
-                                <Text
-                                    numberOfLines={1}
-                                    style={[styles.trackArtistTrack, { marginTop: 1 }]}
-                                >
-                                    {activeTrack.artist}
-                                </Text>
-                            )}
-                            {/* CONTROLS  */}
+                            <PlayerProgressBar style={{ marginTop: 2 }} />
+                            <PlayerControls style={{ marginTop: 40 }} />
 
                         </View>
-                        <PlayerProgressBar style={{ marginTop: 2 }} />
-                        <PlayerControls style={{ marginTop: 40 }} />
-
-                    </View>
-                    <PlayerVolumeBar style={{ marginTop: 40, marginBottom: 30 }} />
-                    <View style={utilsStyles.centeredRow}>
-                        <PlayerToggleRepeat size={30} style={{ marginTop: 6 }} />
+                        <PlayerVolumeBar style={{ marginTop: 40, marginBottom: 30 }} />
+                        <View style={utilsStyles.centeredRow}>
+                            <PlayerToggleRepeat size={30} style={{ marginTop: 6 }} />
+                        </View>
                     </View>
                 </View>
             </View>
-        </View>
+        </LinearGradient>
+
     )
 }
 
---
+++ hooks/usePlayerBackground.ts
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react"
+import { IOSImageColors } from 'react-native-image-colors/build/types'
+import { getColors } from 'react-native-image-colors'
+import { colors } from "@/constants/tokens"
+export const usePlayerBackground = (imageUrl: string) => {
+    const [imageColors, setImageColors] = useState<IOSImageColors | null>(null)
+
+    useEffect(() => {
+        getColors(imageUrl, {
+            fallback: colors.background,
+            cache: true,
+            key: imageUrl,
+        }).then(
+            (colors) => setImageColors(colors as IOSImageColors)
+        )
+    }, [getColors])
+
+    return { imageColors }
+}
