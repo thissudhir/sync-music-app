8442e9954d92e77f9fb78eeea14ba36a6eeab3a4
Abhishek <abhishekworkhere@gmail.com>
2026-01-21 23:38:13 +0530

    UPDATE!: used the favorite hook
    BREAKING CHANGE: used the favorite hook

--- app/(tabs)/favorites/index.tsx
+++ app/(tabs)/favorites/index.tsx
@@ -5,19 +5,24 @@ import { ScrollView, StyleSheet, Text, View } from 'react-native'
 import library from '@/assets/data/library.json'
 import { screenPadding } from '@/constants/tokens'
 import useNavigationSearch from '@/hooks/useNavigationSearch'
+import { useFavorite } from '@/store/library'
+import { trackTitleFilter } from '@/helpers/filter'
 const FavoritesScreen = () => {
     const search = useNavigationSearch({
         searchBarOptions: {
             placeholder: "Find in favorite"
         }
     })
-    const favoriteTracks = useMemo(() => {
-        return library.filter(track => track.rating === 1)
-    }, [])
+    const favoriteTracks = useFavorite().favroite
+    const filteredFavoriteTracks = useMemo(() => {
+        if (!search) return favoriteTracks
+        return favoriteTracks.filter(trackTitleFilter(search))
+    }, [search, favoriteTracks])
+
     return (
         <View style={defaultStyles.container}>
             <ScrollView style={{ paddingHorizontal: screenPadding.horizontal }} contentInsetAdjustmentBehavior='automatic'>
-                <TrackList scrollEnabled={false} track={favoriteTracks} />
+                <TrackList scrollEnabled={false} track={filteredFavoriteTracks} />
             </ScrollView>
             <Text style={defaultStyles.text}>FavoritesScreen</Text>
         </View>
