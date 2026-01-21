5e9e75a4cbbf4bc42e9b955625ac0a8c5768d873
Abhishek <abhishekworkhere@gmail.com>
2026-01-21 22:53:37 +0530

    ADD(hook): for using the constant JSON

--- store/library.tsx
+++ store/library.tsx
@@ -1,5 +1,23 @@
 import { TrackWithPlaylist } from "@/helpers/types";
-
+import { Track } from "react-native-track-player";
+import { create } from 'zustand'
+import library from '@/assets/data/library.json'
 interface LibraryState {
     tracks: TrackWithPlaylist[]
+    toggleTrackFavorite: (track: Track) => void
+    addToPlaylist: (track: Track, playlistName: string) => void
+}
+
+export const useLibraryStore = create<LibraryState>()((set) => ({
+    tracks: library,
+    toggleTrackFavorite: () => { },
+    addToPlaylist: () => { }
+}))
+
+export const useTracks = () => useLibraryStore((state) => state.tracks)
+
+export const useFavorite = () => {
+    const favroite = useLibraryStore((state) => state.tracks.filter((track) => track.rating === 1))
+    const toggleTrackFavorite = useLibraryStore(state => state.toggleTrackFavorite)
+    return { toggleTrackFavorite, favroite  }
 }
